ab22f70550aee05e2cc94e4417a4fa59585398b3
T A L I K A <159610428+Talikamuhib@users.noreply.github.com>
2026-01-06 18:40:24 +0000

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+import sys
+
+# Get the absolute path of the current file
+FILE = Path(__file__).resolve()
+# Get the parent directory of the current file
+ROOT = FILE.parent
+# Add the root path to the sys.path list if it is not already there
+if ROOT not in sys.path:
+    sys.path.append(str(ROOT))
+# Get the relative path of the root directory with respect to the current working directory
+ROOT = ROOT.relative_to(Path.cwd())
+
+# Sources
+IMAGE = 'Image'
+
+
+SOURCES_LIST = [IMAGE]
+
+# Images config
+IMAGES_DIR = ROOT / 'images'
+DEFAULT_IMAGE = IMAGES_DIR / 'STONE- (15).jpg'
+DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'STONE 15.jpg'
+
+# Videos config
+VIDEO_DIR = ROOT / 'videos'
+VIDEOS_DICT = {
+    'video_1': VIDEO_DIR / 'video_1.mp4',
+    'video_2': VIDEO_DIR / 'video_2.mp4',
+    'video_3': VIDEO_DIR / 'video_3.mp4',
+}
+
+# ML Model config
+MODEL_DIR = ROOT / 'weights'
+
+DETECTION_MODEL = MODEL_DIR / 'best.pt'
+# In case of your custome model comment out the line above and
+# Place your custom model pt file name at the line below 
+# DETECTION_MODEL = MODEL_DIR / 'my_detection_model.pt'
+
+DETECTION_MODEL_lite = MODEL_DIR / 'best.pt(1)'
+
+# Webcam
+WEBCAM_PATH = 0
